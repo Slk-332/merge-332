ac8c5dc7b738944fb46b2399a04c1b657d08ddf0
Slk-332 <tanawat.slk@gmail.com>
2024-07-31 10:20:13 +0700

    modified surname variable and surname text defauit

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,17 +4,26 @@
     <q-form
       @submit="onSubmit"
       @reset="onReset"
-      class="q-gutter-md"
+      class="q -gutter-md"
     >
+       <!-- name -->
       <q-input
-        filled
+        filled 
         v-model="name"
         label="Your name *"
-        hint="Name and surname"
+        hint="Name"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        :rules="[ val => val && val.length > 0 || 'Please type yourname']"
       />
-
+        <!-- surname -->
+      <q-input
+        filled
+        v-model="surname"
+        label="Your surname *"
+        hint="Surname"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type yoursurname']"
+      />
       <q-input
         filled
         type="number"
@@ -46,12 +55,14 @@ export default {
   setup () {
     const $q = useQuasar()
 
-    const name = ref(null)
+    const name = ref(ธนวัฒน์)
+    const surname = ref(ศรีลาคำ)
     const age = ref(null)
     const accept = ref(false)
 
     return {
       name,
+      surname,
       age,
       accept,
 
